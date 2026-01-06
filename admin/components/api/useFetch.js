51ceb05b2c27ffefbae6ce16f8d791cdd8c92d2e
Arnav Gupta <info@arnavgupta.net>
2026-01-06 22:47:01 +0530

    feat: debug

--- admin/components/api/useFetch.js
+++ admin/components/api/useFetch.js
@@ -46,7 +46,7 @@ export default function useFetch() {
 
       // Refresh token
       const [new_fingerprint, uuid] = generateFingerPrint();
-      const refreshRes = await fetch(`${base}/auth/update-token`, {
+      const refreshRes = await fetch(`${base}/v1/admin/auth/update-token`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
