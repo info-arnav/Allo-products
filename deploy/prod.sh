613e016c2e6fdcdb509dfbc3aff84928cd1ab3c6
Arnav Gupta <info@arnavgupta.net>
2026-01-09 03:24:49 +0530

    feat: syncing

--- deploy/prod.sh
+++ deploy/prod.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+docker system prune -af
 set -e
 
 cd /var/www/Allo-products
 
 git checkout main
 git pull origin main
+
+echo "Syncing database..."
+cd server
+ENV=production npm run sync
+cd ..
 
 docker compose -f docker-compose.production.yml down
 docker compose -f docker-compose.production.yml up -d --build
--- deploy/staging.sh
+++ deploy/staging.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+docker system prune -af
 set -e
 
 cd /var/www/Allo-products
 
 git checkout deploy
 git pull origin deploy
+
+echo "Syncing database..."
+cd server
+ENV=staging npm run sync
+cd ..
 
 docker compose -f docker-compose.staging.yml down
 docker compose -f docker-compose.staging.yml up -d --build
