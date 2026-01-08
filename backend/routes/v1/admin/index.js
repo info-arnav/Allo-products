6e4b48e957a18bf5d5fe48cd0e6b4f748d677b65
Arnav Gupta <info@arnavgupta.net>
2026-01-08 20:09:39 +0530

    feat: check new structure

--- backend/routes/v1/admin/index.js
+++ backend/routes/v1/admin/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const authRoutes = require("./auth");
-const apiRoutes = require("./api/user");
+const apiRoutes = require("./api");
 
 router.use("/auth", authRoutes);
 router.use("/api", apiRoutes);
--- backend/routes/v1/index.js
+++ backend/routes/v1/index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const userRoutes = require("./user");
-// const shopRoutes = require("./shop");
+const shopRoutes = require("./shop");
 const adminRoutes = require("./admin");
 
 router.use("/user", userRoutes);
-// router.use("/shop", shopRoutes);
+router.use("/shop", shopRoutes);
 router.use("/admin", adminRoutes);
 
 module.exports = router;
