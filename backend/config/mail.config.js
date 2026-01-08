0bbb7bda39b87cebc69bcda42a5b782aa391c66d
Arnav Gupta <info@arnavgupta.net>
2026-01-08 17:09:49 +0530

    feat: switched to aws ses

--- backend/config/mail.config.js
+++ backend/config/mail.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   EMAIL: process.env.EMAIL,
   NAME: process.env.NAME,
-  AWS_ACCESS_KEY_ID: process.env.SMTP_USERNAME,
-  AWS_SECRET_ACCESS_KEY: process.env.SMTP_PASSWORD,
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
   AWS_REGION: process.env.AWS_REGION,
 };
